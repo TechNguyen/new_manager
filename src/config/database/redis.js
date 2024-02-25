3f2b89b0b35330cd8857c3b7f230249fff6b605e
TechNguyen <ndt13102003@gmail.com>
2024-02-26 00:28:17 +0700

    111

--- src/config/database/redis.js
+++ src/config/database/redis.js
@@ -1,20 +1,5 @@
 import redis from 'redis'
 
-
-const client = redis.createClient({
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT,
-    username: process.env.REDIS_USER,
-    password: process.env.REDIS_PASSWORD
-  });
+const client = new Redis('rediss://red-cndj5s6n7f5s73blj3m0:gtsPFzBiA3wUMxWuYT1JASM1vLmwcKhW@frankfurt-redis.render.com:6379');
   
-// Event listener to handle Redis errors
-client.on('error', (err) => {
-    console.error('Redis error:', err);
-});
-
-client.on('connect', (err) => {
-    console.error('Redis connect');
-});
-
 export default client
